f6e7ec24d1d1597d518b11c3fb21a6573ce41271
Ayush Kholiya <52167169+ayushk048@users.noreply.github.com>
2020-10-10 15:43:50 +0530

    Add files via upload

---
+++ precache-manifest.ea709d96754e72db9ff83dd05e060808.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f709733da986d12dffb9de4f8cf336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8da7aa850bf8d9b2f58",
+    "url": "/static/css/main.38cf1bcc.chunk.css"
+  },
+  {
+    "revision": "379db8cf3d02686ccc9b",
+    "url": "/static/js/2.eab6bace.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eab6bace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8da7aa850bf8d9b2f58",
+    "url": "/static/js/main.e0551201.chunk.js"
+  },
+  {
+    "revision": "f6016a037164a28cadb8",
+    "url": "/static/js/runtime-main.cc937f93.js"
+  },
+  {
+    "revision": "e586b52052a8bd085689cf8c2cdd2a98",
+    "url": "/static/media/weather--01.e586b520.jpg"
+  },
+  {
+    "revision": "9806c3f9387d39658c45507f7872617f",
+    "url": "/static/media/weather--02.9806c3f9.jpg"
+  },
+  {
+    "revision": "d6b265e928c600b5a4f41bbb6ed6bc4a",
+    "url": "/static/media/weather--06.d6b265e9.jpg"
+  },
+  {
+    "revision": "042657e5df79054d0b8228edd04f1552",
+    "url": "/static/media/weather--08.042657e5.jpg"
+  },
+  {
+    "revision": "5a04c0555a188c7b97bf5e5a2e4ab98d",
+    "url": "/static/media/weather-full-08.5a04c055.jpg"
+  },
+  {
+    "revision": "777f5a64ad4ab96e3e9998358450f705",
+    "url": "/static/media/weather-full-09.777f5a64.jpg"
+  },
+  {
+    "revision": "4f4bd0fdb21878e1d7da40c0f5055923",
+    "url": "/static/media/weather-full-10.4f4bd0fd.jpg"
+  },
+  {
+    "revision": "2e3dd3efc674663a27e02d48ee0d4569",
+    "url": "/static/media/weather-full-11.2e3dd3ef.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f254449263d21e045746bba1cc092b1.js"
+  "/precache-manifest.ea709d96754e72db9ff83dd05e060808.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
